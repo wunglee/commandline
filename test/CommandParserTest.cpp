38719e5694f435c47145b5849c8df9ebb10931ce
wunglee <wunglee@qq.com>
2021-05-04 17:37:40 +0800

    加入TEST(单个命令,整型命令)，一般化提炼testCommandValue

--- test/CommandParserTest.cpp
+++ test/CommandParserTest.cpp
@@ -1,20 +1,21 @@
 #include "gtest/gtest.h"
 #include "../src/CommandParser.h"
 #include "../src/Exceptions.h"
-void testStringValueCommand(const char *commandline, const char *flag, const char *value){
+template <typename T>
+void testCommandValue(const char *commandline, const char *flag, const T value){
     CommandParser commandParser;
     Command result = commandParser.parse(commandline);
     ASSERT_EQ(result.flag_, flag);
-    ASSERT_EQ(boost::any_cast<std::string>(result.value_), value);
+    ASSERT_EQ(boost::any_cast<T>(result.value_), value);
 }
 TEST(单个命令,字符型命令){
-    testStringValueCommand("-l local/usr", "l", "local/usr");
+    testCommandValue<std::string>("-l local/usr", "l", "local/usr");
 }
 TEST(单个命令,字符型命令允许在flag和value之间存在多个空格){
-    testStringValueCommand("-l   local/usr", "l", "local/usr");
+    testCommandValue<std::string>("-l   local/usr", "l", "local/usr");
 }
 TEST(单个命令,字符型命令允许在commandline前后存在多个空格){
-    testStringValueCommand("   -l   local/usr   ", "l", "local/usr");
+    testCommandValue<std::string>("   -l   local/usr   ", "l", "local/usr");
 }
 void testInvalidCommandLineException(const char *commandline){
     CommandParser commandParser;
@@ -24,4 +25,7 @@ TEST(单个命令,字符型命令是否以横线开头){
     testInvalidCommandLineException("l local/usr");
     testInvalidCommandLineException("+l local/usr");
     testInvalidCommandLineException("   l     local/usr   ");
+}
+TEST(单个命令,整型命令){
+    testCommandValue<int>("-p 8080", "p", 8080);
 }
