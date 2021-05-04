2a73c0117e6a687fbb884b1c99fef9b6d933a927
wunglee <wunglee@qq.com>
2021-05-04 16:57:46 +0800

    TEST(单个命令,字符型命令是否以横线开头)

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 using namespace boost;
 Command CommandParser::parse(std::string commandLine) {
-    vector<string> commandsTokens;
-    boost::split_regex( commandsTokens, commandLine, regex(" "));
     if(commandLine.find("-", 0)!=0){
         throw InvalidCommandLineException("命令行没有以'-'开头");
     }
+    vector<string> commandsTokens;
+    boost::split_regex( commandsTokens, commandLine, regex(" "));
     string flag = commandsTokens[0].substr(1);
     string value = commandsTokens[1];
     return Command(flag,value);
