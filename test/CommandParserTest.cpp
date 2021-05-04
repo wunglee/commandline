53ea6d367f5d63d1ff6fb8249f65cd7eb8833bc7
wunglee <wunglee@qq.com>
2021-05-04 17:08:57 +0800

    重构测试代码，提炼testStringValueCommand方法

--- test/CommandParserTest.cpp
+++ test/CommandParserTest.cpp
@@ -1,20 +1,20 @@
 #include "gtest/gtest.h"
 #include "../src/CommandParser.h"
 #include "../src/Exceptions.h"
-TEST(单个命令,字符型命令){
+void testStringValueCommand(const char *commandline, const char *flag, const char *value){
     CommandParser commandParser;
-    Command result = commandParser.parse("-l local/usr");
-    ASSERT_EQ(result.flag_,"l");
-    ASSERT_EQ(boost::any_cast<std::string>(result.value_),"local/usr");
+    Command result = commandParser.parse(commandline);
+    ASSERT_EQ(result.flag_, flag);
+    ASSERT_EQ(boost::any_cast<std::string>(result.value_), value);
+}
+TEST(单个命令,字符型命令){
+    testStringValueCommand("-l local/usr", "l", "local/usr");
+}
+TEST(单个命令,字符型命令允许在flag和value之间存在多个空格){
+    testStringValueCommand("-l   local/usr", "l", "local/usr");
 }
 TEST(单个命令,字符型命令是否以横线开头){
     CommandParser commandParser;
     ASSERT_THROW(commandParser.parse("l local/usr"),InvalidCommandLineException);
     ASSERT_THROW(commandParser.parse("+l local/usr"),InvalidCommandLineException);
-}
-TEST(单个命令,字符型命令允许在flag和value之间存在多个空格){
-    CommandParser commandParser;
-    Command result = commandParser.parse("-l    local/usr");
-    ASSERT_EQ(result.flag_,"l");
-    ASSERT_EQ(boost::any_cast<std::string>(result.value_),"local/usr");
 }
