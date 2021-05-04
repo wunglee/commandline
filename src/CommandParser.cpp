48ad8a08bca1077de3b626658a8b828d86b83879
wunglee <wunglee@qq.com>
2021-05-04 17:12:33 +0800

    TEST(单个命令,字符型命令允许在commandline前后存在多个空格)

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -1,9 +1,11 @@
 #include "CommandParser.h"
 #include "Exceptions.h"
 #include <boost/algorithm/string/regex.hpp>
+#include <boost/algorithm/string/trim.hpp>
 using namespace std;
 using namespace boost;
 Command CommandParser::parse(std::string commandLine) {
+    trim(commandLine);
     if(commandLine.find("-", 0)!=0){
         throw InvalidCommandLineException("命令行没有以'-'开头");
     }
--- test/CommandParserTest.cpp
+++ test/CommandParserTest.cpp
@@ -13,6 +13,9 @@ TEST(单个命令,字符型命令){
 TEST(单个命令,字符型命令允许在flag和value之间存在多个空格){
     testStringValueCommand("-l   local/usr", "l", "local/usr");
 }
+TEST(单个命令,字符型命令允许在commandline前后存在多个空格){
+    testStringValueCommand("   -l   local/usr   ", "l", "local/usr");
+}
 TEST(单个命令,字符型命令是否以横线开头){
     CommandParser commandParser;
     ASSERT_THROW(commandParser.parse("l local/usr"),InvalidCommandLineException);
