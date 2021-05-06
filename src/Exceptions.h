5cba8c7df1a472b9b2cf728bc6de1f21d35c59dd
wunglee <wunglee@qq.com>
2021-05-06 11:04:33 +0800

    格式化异常消息

--- src/Exceptions.h
+++ src/Exceptions.h
@@ -3,7 +3,7 @@
 #include <string>
 class Exception {
 public:
-    std::string message_;
+    const std::string message_;
     Exception(const std::string &message) : message_(message) {}
 };
 class CommandNotFoundException : public Exception {
